58ca7319adba16d24f36168ad32c3e9bf2f72844
diegofreitas <diego.lins.freitas@gmail.com>
2013-07-27 11:02:48 -0400

    first version

---
+++ tcc_android_pattern/tcc_android_patterns.mm
@@ -0,0 +1,396 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1374194769528" ID="ID_1847884920" MODIFIED="1374197870062" TEXT="Monografia">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1374195240962" ID="ID_975254974" MODIFIED="1374197456130" POSITION="right" STYLE="fork" TEXT="Introdu&#xe7;&#xe3;o">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Sugest&#227;o 1 (um ou dois par&#225;grafos por item)
+    </p>
+    <p>
+      &#8211; Identifica a &#225;rea de interesse (palavras do t&#237;tulo)
+    </p>
+    <p>
+      &#8211; Contexto: revis&#227;o b&#225;sica do estado-da-arte
+    </p>
+    <p>
+      &#8211; Prop&#243;sito: e/ou hip&#243;tese sendo investigada
+    </p>
+    <p>
+      &#8211; O prop&#243;sito desse trabalho &#233; definir...
+    </p>
+    <p>
+      &#8211; Esse trabalho prop&#245;e tr&#234;s m&#233;todos para...
+    </p>
+    <p>
+      &#8211; Solu&#231;&#227;o a ser detalhada
+    </p>
+    <p>
+      &#8211; Caracter&#237;stica fundamental, t&#233;cnica ou
+    </p>
+    <p>
+      metodologia, vantagens
+    </p>
+    <p>
+      &#8211; Organiza&#231;&#227;o
+    </p>
+    <p>
+      &#8226; Sugest&#227;o 2 (um ou dois par&#225;grafos por item)
+    </p>
+    <p>
+      &#8211; Contexto, motiva&#231;&#227;o
+    </p>
+    <p>
+      &#8211; O problema em quest&#227;o
+    </p>
+    <p>
+      &#8211; Trabalhos anteriores relacionados (limita&#231;&#245;es)
+    </p>
+    <p>
+      &#8211; Lista de contribui&#231;&#245;es, resultados principais
+    </p>
+    <p>
+      &#8211; Organiza&#231;&#227;o
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1374195329015" ID="ID_260966772" MODIFIED="1374201076441" TEXT="Contexto&amp;Motiva&#xe7;&#xe3;o">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 0in; text-indent: 0.49in" lang="pt-BR" class="western" align="justify">
+      <font color="#000000">descreve o contexto hist&#243;rico em torno do problema, o que sabemos, o que foi feito anteriormente, as perguntas sem respostas e quest&#245;es centrais do problema</font>
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="messagebox_warning"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1374195352240" ID="ID_1116671874" MODIFIED="1374197559256" TEXT="Contribui&#xe7;&#xe3;o ">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      como este trabalho contribui para a&#160;&#160;engenharia de software
+    </p>
+    <p>
+      O trabalho vai melhorar o que
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      N&#227;o tem refer&#234;ncias formais sobre o assunto.
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_cancel"/>
+<icon BUILTIN="full-6"/>
+</node>
+<node CREATED="1374195445376" ID="ID_607595910" MODIFIED="1374337202468" TEXT="Delimitar o Tema">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Design Principles
+    </p>
+    <p>
+      Coes&#227;o: Beneficios
+    </p>
+    <p>
+      Padr&#227;o de projetos
+    </p>
+    <p>
+      Camada apresenta&#231;&#227;o
+    </p>
+    <p>
+      MVC/MVP
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="messagebox_warning"/>
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1374195575254" ID="ID_1202356404" MODIFIED="1374196243335" TEXT="Justificativa">
+<icon BUILTIN="button_cancel"/>
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1374195886126" ID="ID_1603731742" MODIFIED="1374196273650" TEXT="Organiza&#xe7;&#xe3;o">
+<icon BUILTIN="button_cancel"/>
+<icon BUILTIN="full-7"/>
+</node>
+<node CREATED="1374195915748" ID="ID_1395101876" MODIFIED="1374197918884" TEXT="Trabalhos Relacionados">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#8226; Trabalhos anteriores relacionados
+    </p>
+    <p>
+      &#8226; No mesmo contexto, n&#227;o resolvem o problema ou
+    </p>
+    <p>
+      apresentam apenas solu&#231;&#245;es parciais
+    </p>
+    <p>
+      &#8226; Extens&#227;o ou continua&#231;&#227;o de um trabalho anterior: deve
+    </p>
+    <p>
+      ser mencionado na introdu&#231;&#227;o
+    </p>
+    <p>
+      &#8226; Agrupar trabalhos similares e detalhar um ou dois:
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_cancel"/>
+<icon BUILTIN="full-5"/>
+</node>
+<node CREATED="1374195966557" ID="ID_1718219245" MODIFIED="1374201424830" TEXT="Problema">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      O problema em quest&#227;o
+    </p>
+    <p>
+      &#8226; Defini&#231;&#227;o do problema
+    </p>
+    <p>
+      &#8226; Sua import&#226;ncia, relev&#226;ncia, aplica&#231;&#245;es pr&#225;ticas
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      O problema dentro da engenharia de software que pode acontecer em projetos android.
+    </p>
+    <p>
+      Muitos erros, Retrabalho, manuten&#231;&#227;o dif&#237;cil.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Definir Hip&#243;tese
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_cancel"/>
+<icon BUILTIN="full-2"/>
+</node>
+</node>
+<node CREATED="1374195623742" FOLDED="true" ID="ID_1053234259" MODIFIED="1374199179890" POSITION="left" TEXT="Resumo">
+<icon BUILTIN="messagebox_warning"/>
+<node CREATED="1374196046820" ID="ID_22471074" MODIFIED="1374196067870" TEXT="Escopo do trabalho">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1374196046822" ID="ID_1892938633" MODIFIED="1374196065217" TEXT="Principais objetivos">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1374196046823" ID="ID_49798489" MODIFIED="1374196063197" TEXT="Principal resultado ou conclus&#xe3;o">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+<node CREATED="1374195654830" ID="ID_1324712738" MODIFIED="1374201344038" POSITION="right" TEXT="Conclus&#xe3;o">
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1374195693344" ID="ID_887788822" MODIFIED="1374195743041" TEXT="Tema Apresentado">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1374195746021" ID="ID_59943712" MODIFIED="1374201371857" TEXT="Principais Resultados">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="margin-bottom: 0in; text-indent: 0.49in" lang="pt-BR" class="western" align="justify">
+      <font color="#000000">Que conclus&#245;es se podem tirar? </font>
+    </p>
+    <p style="margin-bottom: 0in; text-indent: 0.49in" lang="pt-BR" class="western" align="justify">
+      <font color="#000000">Que influ&#234;ncias tivemos nos resultados? </font>
+    </p>
+    <p style="margin-bottom: 0in; text-indent: 0.49in" lang="pt-BR" class="western" align="justify">
+      <font color="#000000">O que os dados realmente dizem? </font>
+    </p>
+    <p style="margin-bottom: 0in; text-indent: 0.49in" lang="pt-BR" class="western" align="justify">
+      <font color="#000000">H&#225; ambig&#252;idades na nossa interpreta&#231;&#227;o? </font>
+    </p>
+    <p style="margin-bottom: 0in; text-indent: 0.49in" lang="pt-BR" class="western" align="justify">
+      <font color="#000000">Podemos dar outras explica&#231;&#245;es para os dados que vemos? </font>
+    </p>
+    <p style="margin-bottom: 0in; text-indent: 0.49in" lang="pt-BR" class="western" align="justify">
+      <font color="#000000">Nossos resultados s&#227;o realmente cr&#237;veis?</font>
+    </p>
+    <p style="margin-bottom: 0in; text-indent: 0.49in" lang="pt-BR" class="western" align="justify">
+      <font color="#000000">Qual o significado pr&#225;tico dos resultados? </font>
+    </p>
+    <p style="margin-bottom: 0in; text-indent: 0.49in" lang="pt-BR" class="western" align="justify">
+      <font color="#000000">Se esses resultados se revelaram gerais e o que os t&#233;cnicos ou desenvolvedores podem fazer com eles? </font>
+    </p>
+    <p style="margin-bottom: 0in; text-indent: 0.49in" lang="pt-BR" class="western" align="justify">
+      <font color="#000000">Certifique-se de ter dado informa&#231;&#245;es suficientes para outras pessoas para ajud&#225;-las a repetir o estudo, se quiserem.</font>
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1374195768208" ID="ID_795545764" MODIFIED="1374195779038" TEXT="Trabalhos Futuros">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+<node CREATED="1374196305932" ID="ID_974812849" MODIFIED="1374199174872" POSITION="left" TEXT="Objetivos">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Analisar ..........................................&lt;
+    </p>
+    <p>
+      Objeto do estudo
+    </p>
+    <p>
+      &gt;
+    </p>
+    <p>
+      Especifica entidades que ser&#227;o estudadas ao longo do
+    </p>
+    <p>
+      experimento.
+    </p>
+    <p>
+      Com o prop&#243;sito de........................&lt;
+    </p>
+    <p>
+      Objetivo
+    </p>
+    <p>
+      &gt;
+    </p>
+    <p>
+      Define a inten&#231;&#227;o da realiza&#231;&#227;o do experimento. Geralmente
+    </p>
+    <p>
+      &#233; escolhida da lista enumerada: caracterizar, avaliar,
+    </p>
+    <p>
+      predizer, controlar, ou melhorar.
+    </p>
+    <p>
+      Com respeito a ...............................&lt;
+    </p>
+    <p>
+      O foco da qualidade
+    </p>
+    <p>
+      &gt;
+    </p>
+    <p>
+      Indica o principal aspecto da qualidade que est&#225; sendo
+    </p>
+    <p>
+      estudado, por exemplo, efici&#234;ncia, confiabilidade,
+    </p>
+    <p>
+      produtividade.
+    </p>
+    <p>
+      Do ponto de vista ...
+    </p>
+    <p>
+      ............
+    </p>
+    <p>
+      ............&lt;
+    </p>
+    <p>
+      Perspectiva
+    </p>
+    <p>
+      &gt;
+    </p>
+    <p>
+      Especifica o ponto de vista que os resultados do
+    </p>
+    <p>
+      experimento ser&#227;o interpretados, por exemplo,
+    </p>
+    <p>
+      desenvolvedor, consumidor, gerente.
+    </p>
+    <p>
+      No contexto de ...............................&lt;
+    </p>
+    <p>
+      Contexto
+    </p>
+    <p>
+      &gt;
+    </p>
+    <p>
+      Especifica o ambiente onde o experimento est&#225; sendo
+    </p>
+    <p>
+      executado.
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1374196313292" FOLDED="true" ID="ID_1765684065" MODIFIED="1374202730690" POSITION="right" TEXT="Metodologia">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      : O experimento ser&#225; realizado no laborat&#243;rio
+    </p>
+    <p>
+      &#160;Defini&#231;&#227;o das entidades envolvidas nos experimento software ou componentes
+    </p>
+    <p>
+      &#160;caracteriza&#231;&#227;o das entidades vers&#227;o em que est&#225; dispon&#237;vel, aplicativos que faz x yz
+    </p>
+    <p>
+      Quantos aplicativos e quais.
+    </p>
+    <p>
+      &#160;defini&#231;&#227;o da forma de coleta das informa&#231;&#245;es
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1374197091320" ID="ID_1608434845" MODIFIED="1374197107451" TEXT="Ferramentas"/>
+<node CREATED="1374197108065" ID="ID_1632564238" MODIFIED="1374197114598" TEXT="Documenta&#xe7;&#xe3;o"/>
+<node CREATED="1374197115679" ID="ID_144912103" MODIFIED="1374197134169" TEXT="Implementa&#xe7;&#xe3;o"/>
+<node CREATED="1374197139599" ID="ID_377635483" MODIFIED="1374197142391" TEXT="M&#xe9;tricas"/>
+<node CREATED="1374197150599" ID="ID_580782484" MODIFIED="1374197154580" TEXT="Analise"/>
+</node>
+<node CREATED="1374196347134" ID="ID_1846384544" MODIFIED="1374196353252" POSITION="right" TEXT="Revis&#xe3;o Literatura"/>
+<node CREATED="1374196359128" ID="ID_1083276295" MODIFIED="1374196362482" POSITION="left" TEXT="Experimentos"/>
+<node CREATED="1374197872129" ID="ID_292270892" MODIFIED="1374197879167" POSITION="left" TEXT="Resultados"/>
+</node>
+</map>
